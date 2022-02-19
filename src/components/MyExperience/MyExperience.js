afb27c7707910fb4e2fe110c967f1cf612114fc5
nhtuan1609 <nhtuan1609@gmail.com>
2022-02-19 17:14:10 +0700

    Update information for experience

--- src/components/MyExperience/MyExperience.js
+++ src/components/MyExperience/MyExperience.js
@@ -12,7 +12,31 @@ export default function MyExperience() {
         </Row>
         <Row className="experience-body">
           <Col className="experience-list">
-          <Tilt options={{ max: 10, scale: 1.05 }}>
+            <Tilt options={{ max: 10, scale: 1.05 }}>
+              <div className="experience-item">
+                <div className="experience-item-header">
+                  Emage Development Co., Ltd
+                </div>
+                <div className="experience-item-body">
+                  <h1>Position: Application Developer</h1>
+                  <strong>Teachnology:</strong> MFC, C++ and Python.
+                  <br />
+                  <strong>Duration:</strong> March 2020 - June 2021.
+                  <br />
+                  <strong>Description:</strong>
+                  <br />
+                  - Develop and enhance multiple UI features using MFC and C++.
+                  <br />
+                  - Provided application maintenance while working as Production
+                  Support.
+                  <br />- Research and implement new image processing algorithom
+                  to the application.
+                  <br />- Integrate deep learning method into the application
+                  using CMD or TCP.
+                </div>
+              </div>
+            </Tilt>
+            <Tilt options={{ max: 10, scale: 1.05 }}>
               <div className="experience-item">
                 <div className="experience-item-header">
                   Front-End Web Development Course
@@ -26,41 +50,69 @@ export default function MyExperience() {
                   <br />
                   <strong>Description:</strong>
                   <br />
-                  - Build a front-end website and responsive web design using HTML and CSS.
+                  - Build a front-end website and responsive web design using
+                  HTML and CSS.
                   <br />
                   - Implement a functional front-end website using React.
                   <br />
                   - Consume API RESTful in React.
-                  <br />
-                  - Design the architecture for the website using React and Redux.
+                  <br />- Design the architecture for the website using React
+                  and Redux.
                 </div>
               </div>
             </Tilt>
             <Tilt options={{ max: 10, scale: 1.05 }}>
               <div className="experience-item">
                 <div className="experience-item-header">
-                  Emage Development Co., Ltd
+                  YC Solutions Company Limited
                 </div>
                 <div className="experience-item-body">
-                  <h1>Position: Application Engineer</h1>
-                  <strong>Teachnology:</strong> MFC, C++ and Python.
+                  <h1>Position: Web Developer</h1>
+                  <strong>Teachnology:</strong> JavaScript, TypeScript, Vuejs,
+                  ReactJs, Firebase,...
                   <br />
-                  <strong>Duration:</strong> March 2020 - May 2021.
+                  <strong>Duration:</strong> July 2021 - February 2022.
                   <br />
                   <strong>Description:</strong>
                   <br />
-                  - Develop and enhance multiple UI features  using MFC and C++.
+                  - Develop user interface components of the ERP web application
+                  using Vuejs and Firebase.
                   <br />
-                  - Provided application maintenance while working as Production
-                  Support.
-                  <br />- Research and implement new image processing algorithom
-                  to the application.
-                  <br />- Integrate deep learning method into the application
-                  using CMD or TCP.
+                  - Write high-performance, reusable code for UI components
+                  <br />- Write test scripts, clear and maintainable code.
                 </div>
               </div>
             </Tilt>
-            
+            <Tilt options={{ max: 10, scale: 1.05 }}>
+              <div className="experience-item">
+                <div className="experience-item-header">
+                  freeCodeCamp Certifications
+                </div>
+                <div className="experience-item-body">
+                  <div className="body-container">
+                    <a
+                      className="certification-link"
+                      href="https://www.freecodecamp.org/certification/nhtuan1609/responsive-web-design"
+                    >
+                      View Responsive Web Design Certification
+                    </a>
+                    <a
+                      className="certification-link"
+                      href="https://www.freecodecamp.org/certification/nhtuan1609/javascript-algorithms-and-data-structures"
+                    >
+                      View JavaScript Algorithms and Data Structures
+                      Certification
+                    </a>
+                    <a
+                      className="certification-link"
+                      href="https://www.freecodecamp.org/certification/nhtuan1609/front-end-development-libraries"
+                    >
+                      View Front End Development Libraries Certification
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </Tilt>
           </Col>
         </Row>
       </Container>
